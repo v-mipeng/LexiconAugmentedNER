7dca9d49ae6d7eeed8fccc93bca1f72ffefd8f53
Minlong Peng <v-mipeng@users.noreply.github.com>
2019-08-14 12:57:03 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,1 @@
+python main.py --test data/demo.test.char --modelname demo --savedset data/data.dset
---
+++ train.sh
@@ -0,0 +1,1 @@
+python main.py --train data/demo.train.char --dev data/demo.dev.char --test data/demo.test.char --modelname demo --savedset data/data.dset
